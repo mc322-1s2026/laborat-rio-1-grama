1bc25d1922c9f393b83f655b5266781c8fca209c
Maria Julia Santos Paz <majupaz1711@gmail.com>
2026-03-24 18:52:05 +0000

    Javadoc de Task e User

--- src/main/java/com/nexus/model/Task.java
+++ src/main/java/com/nexus/model/Task.java
@@ -3,24 +3,36 @@ package com.nexus.model;
 import com.nexus.exception.NexusValidationException;
 import java.time.LocalDate;
 
+/** Classe de Tarefa
+ * 
+ * Esta classe possui métodos e atributos relacionados a tarefas a 
+ * serem manipuladas no Nexus. Uma tarefa deve possuir um status e 
+ * sua lógica de transição de estado está implementada. 
+ */
 
 public class Task {
-    // Métricas Globais (Alunos implementam a lógica de incremento/decremento)
     public static int totalTasksCreated = 0;
     public static int totalValidationErrors = 0;
     public static int activeWorkload = 0;
     public int estimatedEffort;
 
     private static int nextId = 1;
-
-    /* "Final" nos atributos que devem ser imutáveis após o nascimento do objeto */
     private final int id;
-    private final LocalDate deadline; // Imutável após o nascimento
+    private final LocalDate deadline;
     private String title;
     private TaskStatus status;
     private User owner;
     private Project projeto;
 
+    /** Construtor de Tarefa
+     * 
+     * Inicializa uma nova Taarefa
+     * 
+     * @param title String - título da Tarefa
+     * @param deadline data limite para realização da Tarefa
+     * @param horas inteiro - horas dedicadas para realização da Tarefa
+     * @param projeto projeto no qual a Tarefa está atribuída
+     */
     public Task(String title, LocalDate deadline, int horas, Project projeto) {
         this.id = nextId++;
         this.deadline = deadline;
@@ -34,18 +46,23 @@ public class Task {
         totalTasksCreated++; 
     }
 
-    /**
-     * Move a tarefa para IN_PROGRESS.
-     * Regra: Só é possível se houver um owner atribuído e não estiver BLOCKED.
+    /** Método para mudar o estado de uma tarefa para IN_PROGRESS
+     * 
+     * Ao mudar o status, o contador activeWorkload é incrementado.
+     * A Tarefa deve possuir um dono {@link User} e não deve estar bloqueada.
+     * Caso contrário, o status da tarefa não mudará e retornará
+     * NexusValidationException {@link NexusValidationException}
+     * 
+     * @param user usuário para quem a tarefa será atribuída
      */
     public void moveToInProgress(User user) {
         if (user == null)
-        { /* Regra 01: Deve haver um owner atribuído: usuário informado deve existir */
+        {
             totalValidationErrors++;
             throw new NexusValidationException("Tarefa precisa ter um dono.");
         }
         if(getStatus() == TaskStatus.BLOCKED)
-        {/* Regra 02: Tarefa não deve estar BLOCKED */
+        {
             totalValidationErrors++;
             throw new NexusValidationException("Tarefa bloqueada: Não é possível alterar seu status.");
         } else
@@ -54,26 +71,34 @@ public class Task {
             this.status = TaskStatus.IN_PROGRESS;
             activeWorkload++;
         }
-
-        // [OK] TODO: Implementar lógica de proteção e atualizar activeWorkload
-        // Se falhar, incrementar totalValidationErrors e lançar NexusValidationException
     }
 
-    /**
-     * Finaliza a tarefa.
-     * Regra: Só pode ser movida para DONE se não estiver BLOCKED.
+    /** Método para mudar o estado de uma tarefa para DONE
+     * 
+     * O contador activeWorkload é decrementado.
+     * Para mudar o estado de uma tarefa, ela não pode estar bloqueada.
+     * Caso contrário, o status da tarefa não mudará e retornará
+     * NexusValidationException {@link NexusValidationException}
      */
     public void markAsDone() {
         if(getStatus() == TaskStatus.BLOCKED)
         {
-            totalValidationErrors++; /* Deveria incrementar isso aqui também? */
+            totalValidationErrors++;
             throw new NexusValidationException("Tarefa bloqueada: Não é possível alterar seu status.");
         }
         this.status = TaskStatus.DONE;
         activeWorkload--;
-        // [OK] TODO: Implementar lógica de proteção e atualizar activeWorkload (decrementar)
     }
 
+    /** Método para mudar o estado de uma tarefa para BLOCKED
+     * 
+     * Para ser bloqueada, uma tarefa não deve possuir o estado DONE.
+     * Caso contrário, o status da tarefa não mudará e retornará
+     * NexusValidationException {@link NexusValidationException}
+     * 
+     * @param blocked Boolean - parâmetro que indica se uma tarefa deve 
+     * ser bloqueada ou não
+     */
     public void setBlocked(boolean blocked) {
         /* Task precisa não estar DONE! */
         if((this.status != TaskStatus.DONE)){
@@ -89,6 +114,10 @@ public class Task {
         }
     }
 
+    /** Método para definir o dono {@link User} da tarefa
+     * 
+     * @param u User - usuário a ser colocado como dono da tarefa
+     */
     public void setOwner(User u){
         this.owner = u;
     }
--- src/main/java/com/nexus/model/User.java
+++ src/main/java/com/nexus/model/User.java
@@ -1,15 +1,25 @@
 package com.nexus.model;
 
-//import com.nexus.model.Task;
-//import java.util.ArrayList;
 import java.util.List;
-//import java.util.Collections;
 import java.util.Objects;
 
+/** Classe de Usuário
+ * 
+ * Esta classe pssui atributos que definem um usuário no Nexus.
+ * Com opção para verificar quantas tarefas {@link Task} estão
+ * atribuídas a este usuário, além de métodos para verificar
+ * seus atributos.
+ */
 public class User {
     private final String username;
     private final String email;
 
+    /** Construtor de Usuário
+     * Inicializa um novo usuário
+     * 
+     * @param username String - nome do usuário
+     * @param email String - email do usuário
+     */
     public User(String username, String email) {
         if (username == null || username.isBlank()) {
             throw new IllegalArgumentException("Username não pode ser vazio.");
@@ -22,25 +32,44 @@ public class User {
         this.email = email;
     }
 
+    /** Override do método equals para Usuário
+     *
+     * Considera-se o email como atributo que diferencia objetos. 
+     * 
+     * @return boolean - True se dois usuários tem a mesma posição
+     * de memória ou tem os mesmos atributos e False caso contário.
+     */
     @Override
     public boolean equals(Object u){
-        if(this == u) return true;  /* Se tiver mesma posição de memória, é igual */
-        if (u == null || getClass () != u.getClass ()) return false ; /* Endereço null ou classes diferentes */
-        /* Considerando email como atributo que diferencia objetos */
+        if(this == u) return true;
+        if (u == null || getClass () != u.getClass ()) return false ; 
         User user = (User) u;
         return this.email == user.email;
     }
 
+    /** Override do método hashCode para Usuário
+     * 
+     * @return hash relacionado ao email do usuário
+     */
     @Override
     public int hashCode(){
         return Objects.hash(email); /* Gera hashCode a partir do email */
     }
 
+    /** Override do método de exibição de Usuário
+     * 
+     * @return nome do usuário 
+     */
     @Override
     public String toString() {
         return this.consultUsername();
     }
 
+    /** Método de verificação de email válido
+     * 
+     * @param email String - email a ser verificado
+     * @return boolean - True caso o email é válido e False caso contrário
+     */
     public static boolean isValidEmailAddress(String email) {
            String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
            java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
@@ -48,22 +77,35 @@ public class User {
            return m.matches();
     }
 
+    /** Método que retorna email de Usuário
+     * 
+     * @return String - email do usuário
+     */
     public String consultEmail() {
         return email;
     }
 
+    /** Método que retorna nome de Usuário
+     * 
+     * @return String - nome do usuário
+     */
     public String consultUsername() {
         return username;
     }
 
+    /** Método que calcula quantas tarefas com estado IN_PROGRESS
+     * atribuídas ao Usuário
+     * 
+     * @param listaDeTarefas lista das tarefas {@link Task} criadas
+     * @return quantidade de tarefas atribuídas ao usuário
+     */
     public long calculateWorkload(List<Task> listaDeTarefas) {
-        long qntdTarefas = 0; /* Contador de Tarefas */
+        long qntdTarefas = 0;
 
-        for(Task task : listaDeTarefas){ /* Iterando a lista de tarefas */
-            /* Contar tasks in progress que pertencem ao usuário */
+        for(Task task : listaDeTarefas){
             if ((task.getStatus() == TaskStatus.IN_PROGRESS) && (this.equals(task.getOwner())))
             {
-                qntdTarefas++; /* Incrementa, caso positivo */
+                qntdTarefas++;
             }
         }
 
